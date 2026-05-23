7d6c8acea57e5f8723291b5145573443ef72d96f
sqweeby <xxcatgirlstink69xx@gmail.com>
2026-05-22 21:46:36 -0700

    feat: drawing in 3D

--- waterthetrees/graphics/Render3D.java
+++ waterthetrees/graphics/Render3D.java
@@ -2,9 +2,9 @@ package waterthetrees.graphics;
 
 public class Render3D extends Render
 {
-    public static final int BITS = 5;
-    public static final int CENTER = 2;
-    public static final double Z_DEPTH = 100.0;
+    public static final int BITS = 15;
+    public static final double CENTER = 2.0;
+    public static final double Z_DEPTH = 8.0;
 
     /**
      * Render 3D is a no args construxtor
@@ -26,19 +26,20 @@ public class Render3D extends Render
     {
         for (int y = 0; y < height; y++)
         {
-            double yDepth = y - height / CENTER;
-            double z = Z_DEPTH / yDepth;
+            double ceiling = (y - height / CENTER) / height;
+            double z = Z_DEPTH / ceiling;
 
             for (int x = 0; x < width; x++)
             {
-                double depth = x - width / CENTER;
+                double depth = (x - width / CENTER) / height;
 
                 depth *= z;
 
-                int xInt = (int) depth & BITS;
+                int xInt = (int) (depth) & BITS;
+                int yInt = (int) (z) & BITS;
                 // converts depth value into integer within bit range
 
-                pixels[x + y * width] = xInt * 128;
+                pixels[x + y * width] = (xInt * 16) | (yInt * 16) << 8;
 
             }
         }
